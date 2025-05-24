6610330b1f92cd96967f0cf7ea253473d17c2406
p0tat0chip <mr.mayankgame@gmail.com>
2025-05-24 16:28:13 +0530

    nothing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,24 @@
 export default {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"], // Update paths based on your project
-    theme: {
-        extend: {
-            fontSize: {
-                sm: "14px",
-                base: "16px",
-                lg: "18px",
-                xl: "20px",
-            },
-        },
+  content: ["./src/**/*.{js,ts,jsx,tsx}"], // Update paths based on your project
+  theme: {
+    extend: {
+      fontSize: {
+        sm: "14px",
+        base: "16px",
+        lg: "18px",
+        xl: "20px",
+      },
     },
-    plugins: [],
+    keyframes: {
+      hoverPulse: {
+        '0%': { transform: 'translateY(0)' },
+        '100%': { transform: 'translateY(-3.5px)' },
+      },
+    },
+    animation: {
+      hoverPulse: 'hoverPulse 1.4s infinite alternate ease-in-out',
+    },
+
+  },
+  plugins: [],
 };
